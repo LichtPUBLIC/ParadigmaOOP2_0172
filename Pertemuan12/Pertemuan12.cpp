219824bf1f1624803ac6cf09f9e8bf9d46a3f17c
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-05-28 10:18:57 +0700

    membuat class seseorang

--- Pertemuan12/Pertemuan12.cpp
+++ Pertemuan12/Pertemuan12.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class seseorang {
+public:
+    // pure virtual function
+    virtual void pesan() = 0;
+    // virtual function biasa
+    // virtual void() {
+    // cout<<"Pesan dari seseorang"<<endl;
+    // }
+};
+
 int main() {
 
 }
