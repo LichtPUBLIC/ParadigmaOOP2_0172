2c0b0cdf51c903b7f0374f969bd400856c677681
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-05-28 10:51:12 +0700

    membuat clas derivedClass

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -9,6 +9,15 @@ public:
     }
 };
 
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived Class";
+
+    }
+};
 
 int main()
 {
